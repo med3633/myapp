ac58a641dd397ffeae900a2a90283472622b7e3d
med363 <blibechmedamine@gmail.com>
2024-03-11 10:16:27 -0700

    serve update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to nginx and go......update
+          Edit no<code>src/App.js</code> and save to nginx and go......update
         </p>
         <a
           className="App-link"
